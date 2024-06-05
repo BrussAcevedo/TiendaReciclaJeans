4de0a2d354aa1419bf41e26cc4edcf46d913e0cd
Bruss Acevedo <bruss.acevedo@gmail.com>
2024-06-05 10:54:08 -0400

    "Metodos y clases (Vacios) Agregados"

--- src/reciclaJeans/ArchivoServicio.java
+++ src/reciclaJeans/ArchivoServicio.java
@@ -1,5 +1,10 @@
 package reciclaJeans;
 
+
 public class ArchivoServicio {
 
+	
+	public static void cargarDatos() {
+		
+	}
 }
--- src/reciclaJeans/ProductoServicio.java
+++ src/reciclaJeans/ProductoServicio.java
@@ -1,5 +1,30 @@
 package reciclaJeans;
 
+import java.util.ArrayList;
+
 public class ProductoServicio {
 
+	private ArrayList<String> listaProducto;
+
+	public ProductoServicio(ArrayList<String> listaProducto) {
+		super();
+		this.listaProducto = listaProducto;
+	}
+
+	public ArrayList<String> getListaProducto() {
+		return listaProducto;
+	}
+
+	public void setListaProducto(ArrayList<String> listaProducto) {
+		this.listaProducto = listaProducto;
+	}
+	
+	public static void listarProductos() {
+		
+	}
+	
+	public static void editarDatos() {
+		
+	}
+	
 }
